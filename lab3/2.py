51754485b9d00862b188929fce43e9e57f7ea385
qazjean <jeanmorqwera@gmail.com>
2025-04-18 18:22:49 +0700

    Update 2.py

--- lab3/2.py
+++ lab3/2.py
@@ -3,7 +3,10 @@ class Person:
         self.name = name
         self.age = age
 
-class Student(Person):
+class Student(Person): #не совсем поняла задание, но, как я поняла, ввести класс студент все же нужно,
+    #тк в атрибутах класса учитель есть список объектов студент.
+    #Так что я скопировала фрагмент кода из прошлого задания, но так же добавила наследование от класса персон
+    #тк ученики тоже люди
     def __init__(self, name, age, student_id):
         super().__init__(name, age)
         self.student_id = student_id
@@ -13,21 +16,19 @@ class Student(Person):
         if 0 <= grade <= 10:
             self.grades.append(grade)
         else:
-            raise ValueError("oh! Такой оценки нет")
+            print(f"oh! оценки {grade} нет")
 
     def get_average(self):
         if len(self.grades) > 0:
             return sum(self.grades) / len(self.grades)
         return 0.0
 
-    def display_info(self):
+    def display_info(self): #выведет информацию о студентах
         print(f"Имя студента: {self.name}")
         print(f"id студента: {self.student_id}")
         print(f"Его средний балл: {self.get_average()}")
-        print(f"Его оценки: {", ".join(map(str, self.grades))}")
+        print(f"Оценки: {', '.join(map(str, self.grades))}")
         print()
-
-
 
 class Teacher(Person):
     def __init__(self, name, age, subject):
@@ -36,14 +37,19 @@ class Teacher(Person):
         self.students = set()
     def add_student(self, student: Student):
         self.students.add(student)
+
     def remove_student(self, student: Student):
-        self.students.remove(student)
+        try:
+            self.students.remove(student)
+        except KeyError: #на всякий случай (нельзя удалить студента, которого и так не было)
+            print(f"Студент {student.name} не найден в списке")
     def list_students(self):
         print(f"Учитель {self.name} ведет {self.subject} у следующих студентов:")
         for s in self.students:
-            s.display_info()
-
-teach = Teacher("Прощаев Александр Аркадьевич", 30, "Базы данных")
+            s.display_info() #для объекта студент используем метод класса студент
+            
+#пример
+teach = Teacher("Прощаев Александр Аркадьевич", 35, "Базы данных")
 student = Student("Лизочка Пронина", 18, 100)
 teach.add_student(student)
 teach.list_students()
