22a5929b9979f67511ef0e4f45ba4be777af4df6
qazjean <jeanmorqwera@gmail.com>
2025-04-18 18:06:27 +0700

    Update 1.py

--- lab3/1.py
+++ lab3/1.py
@@ -4,21 +4,22 @@ class Student:
         self.student_id = student_id
         self.grades = []
     def add_grade(self, grade):
-        if 0 <= grade <= 10:
+        if 0 <= grade <= 10: #на всякий случай проверим 
             self.grades.append(grade)
         else:
-            raise ValueError("oh! Такой оценки нет")
+            print(f"oh! оценки {grade} нет")
     def get_average(self):
-        if len(self.grades) > 0:
+        if len(self.grades) > 0: #если оценок нет, то среднее значение 0
             return sum(self.grades)/len(self.grades)
         return 0.0
-    def display_info(self):
+    def display_info(self): #красиво (на мой взгляд) вывожу информацию о студентах
         print(f"Имя студента: {self.name}")
         print(f"id студента: {self.student_id}")
         print(f"Его средний балл: {self.get_average()}")
-        print(f"Его оценки: {", ".join(map(str, self.grades))}")
+        print(f"Оценки: {', '.join(map(str, self.grades))}")
 
-student = Student("Лизочка Пронина", "1511")
+student = Student("Лизочка Пронина", "1511") #опробуем
 student.add_grade(10)
-student.add_grade(5)
+student.add_grade(6)
+student.add_grade(1000)
 student.display_info()
