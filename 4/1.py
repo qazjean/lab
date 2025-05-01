a3f12e5b2748abd59782d5f408aa25714e2e11ff
qazjean <jeanmorqwera@gmail.com>
2025-05-01 13:33:41 +0700

    Update 1.py

--- 4/1.py
+++ 4/1.py
@@ -5,13 +5,12 @@ class Game():
         self.number = number
         self.time = datetime.datetime.now()
         self.count = 0
-    def trying(self):
+    def trying(self): #метод для угадывания
         log = Log()
         while True:
             y = int(input("Введите ваше число: "))
             self.count += 1
             if y != self.number:
-
                 if y < self.number:
                     print("Ваше число меньше")
                     log.logging(self.count, y,"Меньше")
@@ -24,17 +23,19 @@ class Game():
         print("Угадали")
     def show(self):
         print(f"Вы угадали за {datetime.datetime.now() - self.time}")
-        print(f"Вам понадобилось {self.count} попытки (ок)")
+        print(f"Вам понадобилось {self.count} попытки(ок)")
 
 
 class Log():
     def __init__(self):
-        self.log = open("log.txt", "w", encoding='utf-8')
-    def logging(self, num_try, num, bm):
+        self.log = open("log.txt", "a", encoding='utf-8')
+    def logging(self, num_try, num, bm): #метод для логирования 
         self.num_try = num_try
         self.num = num
         self.bm = bm
-        self.log.write(f"Номер попытки: {self.num_try}, Ваше число: {self.num}, Статус: {self.bm}, Время попытки: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} \n")
+        self.log.write(f"Номер попытки: {self.num_try}, Ваше число: {self.num}, Статус: {self.bm}, Время попытки: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} \n")
+        self.log.flush()  #так как мы сохраняем логирование, я посчитала логичным сохранять не только текущие попытки, но и вообще все
+        #у них все еще отдельная нумерация, так что просмотреть не составит труда
 
 
 
